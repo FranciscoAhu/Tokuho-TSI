0dffca9bb57eae1d192031ecd9b501ebc58aedb4
Ashura91 <fvriquelme2001@gmail.com>
2024-10-01 13:12:47 -0300

    pagina principal y pagina login

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,8 @@
+@extends('home/layouts/master')
+
+@section('contenido-principal')
+    <h3><strong><ins>Publicaciones recientes:</ins></strong></h3>
+    <h3><strong><ins>Videos nuevos:</ins></strong></h3>
+    <h3><strong><ins>Archivos:</ins></strong></h3>
+    <h3><strong><ins>Dojos registrados:</ins></strong></h3>
+@endsection
