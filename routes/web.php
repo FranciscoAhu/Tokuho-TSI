64397b5df3612ac2a3dc895e1bcf905729158d2b
Ashura91 <fvriquelme2001@gmail.com>
2024-10-02 12:43:26 -0300

    vuelvo a subir por un error

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
-
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/',[HomeController::class,'index']);
+route::get('/login',[HomeController::class,'login']);
